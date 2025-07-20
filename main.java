0e63fe0af40c4e05358266448b91dcc4fa9d94e5
Siddhika <Siddhika123@gmail.com>
2025-07-20 16:02:56 +0530

    java file added

---
+++ main.java
@@ -0,0 +1,11 @@
+class hello{
+    public void print(){
+        System.out.println("hello java world");
+    }
+}
+class main{
+    public static void main(String args[]){
+        hello p = new hello();
+        p.print();
+    }
+}
